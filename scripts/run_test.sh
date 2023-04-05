8acf370db9451010b1f72010469de93f6032bb16
h1226933031 <125848749+h1226933031@users.noreply.github.com>
2023-04-05 20:13:25 +0800

    Update run_test.sh

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -9,15 +9,15 @@ python -u run.py \
  --batch_size 32 \
  --n_heads 8 \
  --modes 64 \
- --moving_avg '7 14 28'\
+ --moving_avg '3 7 14 28'\
  --seq_len 30 \
  --label_len 15 \
  --features M \
- --pred_len 15 \
+ --pred_len 16 \
  --e_layers 2 \
  --d_layers 1 \
  --enc_in 34 \
  --dec_in 34 \
  --c_out 34 \
  --itr 1 \
- --train_epochs 1
+ --train_epochs 10
